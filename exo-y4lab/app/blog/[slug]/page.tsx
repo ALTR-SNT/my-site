f57d93a3a9f9683ac84dd8b5a4b0a0b724855e74
Saint Yan <varga.yanosh.05@gmail.com>
2025-06-20 19:21:35 +0300

    fix 3 mvp

--- exo-y4lab/app/blog/[slug]/page.tsx
+++ exo-y4lab/app/blog/[slug]/page.tsx
@@ -1,23 +1,22 @@
 import { getPost } from "@/lib/posts";
 
-
 export async function generateStaticParams() {
   const posts = await import("@/lib/posts").then((m) => m.getPosts());
   return posts.map((post) => ({ slug: post.slug }));
 }
 
-interface PostPageProps {
-  params: {
-    slug: string;
-  };
-}
-
-export default async function PostPage({ params }: PostPageProps) {
-  const post = await getPost(params.slug);
+export default async function PostPage({
+  params,
+}: {
+  params: Promise<{ slug: string }>
+}) {
+  const { slug } = await params
+  const post = await getPost(slug)
+ 
   return (
-    <article className="prose mx-auto p-4">
+    <div>
       <h1>{post.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.contentHtml }} />
-    </article>
-  );
+      <p>{post.contentHtml}</p>
+    </div>
+  )
 }
