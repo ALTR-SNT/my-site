8f62b49961fc332245901344b00e2583564ac5c2
Y.4.N. <106449971+ALTR-SNT@users.noreply.github.com>
2025-08-06 21:35:51 +0300

    fix to Release 1.2

--- exo-y4lab/app/blog/[slug]/page.tsx
+++ exo-y4lab/app/blog/[slug]/page.tsx
@@ -3,34 +3,33 @@ import { client } from '@/sanity/lib/client';
 import { PortableText } from '@portabletext/react';
 import { notFound } from 'next/navigation';
 import Image from 'next/image';
-import type { TypedObject } from '@portabletext/types';
+import { PortableTextBlock } from '@portabletext/types';
 
 interface BlogPost {
   mainImage?: {
-    url: string;
-    [key: string]: unknown;
+    asset?: {
+      url: string;
+    };
   };
   title: string;
-  body: TypedObject[];
+  body: PortableTextBlock[];
   publishedAt: string;
   author?: {
     name: string;
   };
 }
 
-// Тип для props
-interface BlogPostPageProps {
-  params: {
-    slug: string;
-  };
-}
+
+type Props = {
+  params: { slug: string };
+};
 
 const query = groq`
   *[_type == "post" && slug.current == $slug][0]{
     title,
     body,
     publishedAt,
-    mainImage{
+    mainImage {
       asset->{
         url
       }
@@ -41,33 +40,36 @@ const query = groq`
   }
 `;
 
-export default async function BlogPostPage({ params }: BlogPostPageProps) {
-  const post: BlogPost = await client.fetch(query, { slug: params.slug });
+export async function generateStaticParams(): Promise<Props["params"][]> {
+  const slugs = await client.fetch(
+    groq`*[_type == "post" && defined(slug.current)]{ "slug": slug.current }`
+  );
+  return slugs;
+}
 
-  if (!post) {
-    notFound();
-  }
+export default async function BlogPostPage({ params }: Props) {
+  const slug = (await params).slug;
+
+  const post: BlogPost | null = await client.fetch(query, { slug });
+
+  if (!post) return notFound();
 
   return (
-    <article className="prose prose-invert mx-auto">
-      {post.mainImage?.url && (
+    <article>
+      {post.mainImage?.asset?.url && (
         <Image
-          src={post.mainImage.url}
+          src={post.mainImage.asset.url}
           alt={post.title}
-          className="w-full h-auto mb-4 rounded-xl"
           width={800}
           height={400}
+          className="mb-4"
           priority
         />
       )}
-      <h1 className="text-3xl font-bold">{post.title}</h1>
-      {post.author && <p className="text-gray-400">By {post.author.name}</p>}
+      <h1 className="text-3xl">{post.title}</h1>
+      {post.author && <p>By {post.author.name}</p>}
       <p className="text-sm text-gray-500">
-        {new Date(post.publishedAt).toLocaleDateString('uk-UA', {
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        })}
+        {new Date(post.publishedAt).toLocaleDateString()}
       </p>
       <PortableText value={post.body} />
     </article>
